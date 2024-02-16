71cc3ff882afbf10cf4fc9cb02f2c12f7093494d
naharoo <codenarek@gmail.com>
2024-02-16 01:04:27 +0000

    chore: commitlint

--- aaa.js
+++
@@ -1,6 +0,0 @@
-const semver = require("semver");
-const packageJson = require("./package.json");
-
-const newVersion = semver.inc(`${packageJson.version}.0`, "minor").replace(".0", "");
-
-console.log(newVersion);
